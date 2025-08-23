0b83f25905e95674386e6bd69b53ef43d658d7c8
Pamela <junmyeonradpet@gmail.com>
2025-08-22 21:03:58 -0300

    c1

---
+++ src/Produto.java
@@ -0,0 +1,21 @@
+public class Produto {
+    private int codigo;
+    private String nome;
+
+    public Produto(){
+    }
+
+    int getCodigo(){
+        return codigo;
+    }
+    public void setCodigo(int cod){
+        codigo = cod;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String n){
+        nome = n;
+    }
+
+}
