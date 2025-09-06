e86a3ae48f18d0ad2d6f11c956507c168eab7820
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-09-05 21:39:08 -0300

    herança

--- src/App.java
+++ src/App.java
@@ -1,15 +1,12 @@
 
-import entities.Cliente;
-import entities.Pedidos;
-import entities.Produto;
 
 public class App {
     public static void main(String[] args) throws Exception {
         //Produto p = new Produto(); // chamada sem construtor e precisa inicilizar os atributos
-        Produto p1 = new Produto(4, "Console Roxo"); // uso de construtor
-        Cliente c = new Cliente();
-        c.setCPF("70606857400");
-        c.setNome("Pamela");
+        // Produto p1 = new Produto(4, "Console Roxo"); // uso de construtor
+        // Cliente c = new Cliente();
+        // c.setCPF("70606857400");
+        // c.setNome("Pamela");
         
         // System.out.println("Dados do Produto:");
         // System.out.println("Código: " + p1.getCodigo());
@@ -18,11 +15,41 @@ public class App {
         // Circulo cir = new Circulo();
         // cir.setRaio(4.5);
         // System.out.println(cir.toString());
+/* 
 
         Pedidos p = new Pedidos(1, "Tinta", 50.00);
         System.out.println("------- Dados do pedido --------");
         System.out.println("Codigo do pedido: " + p.getCod());
         System.out.println("Nome do produto: " + p.getNome());
         System.out.println("Total do pedido: R$ " + p.getTotal());
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+        Neta n = new Neta();
     }
+}
+class Neta extends Filha {
+    public Neta() {
+        System.out.println("rodando o construtor da classe Neta");
+    }   
+}
+class Filha extends Mae {
+    public Filha() {
+        System.out.println("rodando o construtor da classe Filha");
+    }   
+}
+class Mae {
+    public Mae() {
+        System.out.println("rodando o construtor da classe Mãe");
+    }   
 }
